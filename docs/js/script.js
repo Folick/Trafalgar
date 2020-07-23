f7c11c0e46cbdca7d592a50659591eb355ca10b2
Alexander <folickthebest@gmail.com>
2020-07-23 21:53:50 +0300

    add slider

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,4 +4,12 @@ $(function () {
       e.preventDefault(),
       $(".header__menu-list").toggleClass("is-active")
   });
+  $('.testimonials__slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button id="prev" type="button" class="slick-arrow slick-prev"><img src="images/left-arrow.svg" alt="left"></button>',
+    nextArrow: '<button id="next" type="button" class="slick-arrow slick-next"><img src="images/right-arrow.svg" alt="right"></button>'
+  });
 });
