5334e783e66dafd2fdd33e4fcfb6ec192024592a
Alexander <folickthebest@gmail.com>
2020-07-25 02:56:27 +0300

    Revert "add anchor and scroll to top"
    This reverts commit 690f3ab33639a117d947c6bdbb1cb5fc3d2d78e2.

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,11 +1,4 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 40) {
-      $('#scrollTop').fadeIn();
-    } else {
-      $('#scrollTop').fadeOut();
-    }
-  });
+$(function () {
   $(".hamburger.hamburger--squeeze").on("click", function (e) {
     $(this).toggleClass("is-active"),
       e.preventDefault(),
@@ -18,16 +11,5 @@ $(document).ready(function () {
     slidesToScroll: 1,
     prevArrow: '<button id="prev" type="button" class="slick-arrow slick-prev"><img src="images/left-arrow.svg" alt="left"></button>',
     nextArrow: '<button id="next" type="button" class="slick-arrow slick-next"><img src="images/right-arrow.svg" alt="right"></button>'
-  });
-  $('a[href^="#"]').click(function () {
-    let anchor = $(this).attr('href');
-    $('html, body').animate({
-      scrollTop: $(anchor).offset().top
-    }, 600);
-  });
-  $("#scrollTop").click(function () {
-    $('html ,body').animate({
-      scrollTop: 0
-    }, 800);
   });
 });
